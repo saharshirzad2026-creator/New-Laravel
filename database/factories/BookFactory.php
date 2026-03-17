d74e182d5d704fdd3c74d485ffb48c828051a2cd
saharshirzad2026-creator <sahsrshirzad2026@gmail.com>
2026-03-16 18:52:03 -0700

    bookstore

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            "name"=> $this->faker->sentence(3),
+            "author_name"=> $this->faker->name,
+            "description"=> $this->faker->sentence(6),
+        ];
+    }
+}
